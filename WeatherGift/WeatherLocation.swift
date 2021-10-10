59a9c96fcc075bb21f5f988de9ca64da14d7a5c8
Jose Riera <rieraja@bc.edu>
2021-10-10 14:17:10 -0400

    6.3

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Jose Riera on 10/10/21.
+//
+
+import Foundation
+
+struct WeatherLocation{
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
